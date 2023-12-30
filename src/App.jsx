6b9263aa4e325ea562c22b9e7772719e7c334fda
SHANIB C K <ckshanib8@gmail.com>
2023-12-30 17:12:59 +0530

    Commands added at App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+// This is App.jsx file
+// This is importing section
 import React from "react";
 import HomeScreen from "./components/HomeScreen";
 
 const App = () => {
   return (
     <>
+      {/* This is HomeScreen page */}
       <HomeScreen />
     </>
   );
