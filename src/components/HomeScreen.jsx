b1e71781dfb193435233cc058d435fa3c00099b4
SHANIB C K <ckshanib8@gmail.com>
2023-12-30 16:03:29 +0530

    icon size adjestes

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -10,18 +10,20 @@ const HomeScreen = () => {
   return (
     <>
       <div className="bg-gray-800 w-screen h-screen text-white flex flex-col justify-center items-center">
-        <div className="bg-black w-[310px] sm:w-[800px] bg-opacity-50 rounded-2xl shadow-2xl shadow-blue-800 flex flex-col items-center justify-center m-auto">
-          <h1 className="sm:text-3xl text-2xl font-bold mt-5">
+        <div className="bg-black w-[310px] lg:w-[800px] bg-opacity-50 rounded-2xl shadow-2xl shadow-blue-800 flex flex-col items-center justify-center m-auto">
+          <h1 className="lg:text-3xl text-2xl font-bold mt-5">
             Random Quot <span className="text-red-600">Generator</span>
           </h1>
-          <div className="sm:py-[110px] py-[55px] px-[15px]">{quote.text}</div>
+          <div className="lg:py-[110px] py-[55px] px-[15px]">{quote.text}</div>
           <div>
-            <div className="h-[1.5px] bg-green-800 sm:w-[600px] w-[250px]"></div>
+            <div className="h-[1.5px] bg-green-800 lg:w-[600px] w-[250px]"></div>
             <div className="flex flex-row items-center justify-between my-8 mx-0">
-              <div className="author">{quote.author}</div>
-              <div className="flex flex-row">
-                <IoReloadCircleOutline />
-                <FaXTwitter />
+              <div className="text-[20px] text-white font-extrabold">
+                {quote.author}
+              </div>
+              <div className="flex gap-[40px]">
+                <IoReloadCircleOutline className="cursor-pointer size-8" />
+                <FaXTwitter className="cursor-pointer size-8" />
               </div>
             </div>
           </div>
